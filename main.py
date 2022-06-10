3578b180318393dd12714117aa4cbea12ad0d636
Jacob Sandstrom <jacob.sandstrom@qrtech.se>
2022-06-10 16:19:37 +0200

    .

--- main.py
+++ main.py
@@ -10,72 +10,87 @@ import neural_network as nn
 # Initializing number of dots
 N = 10
 
+NEWRANDOMRATE = 0.8  # 0.75 means 1/4 of of dots are always new
+
 FRAMES = 100
 INTERVAL = 10
 
-XAREA = 100.0
-YAREA = 100.0
+XAREA = 200
+YAREA = 200
 
-XGOAL = 50.0
-YGOAL = 50.0
-RADIUSGOAL = 10.0
+XGOAL = 100
+YGOAL = 100
+RADIUSGOAL = 10
 
 NNSIZE = [2, 64, 128, 64, 2]
 
 # Creating dot class
+
+
 class dot(object):
-    def __init__(self):
-        self.neural_network = nn.Network(NNSIZE)
+  def __init__(self):
+    self.neural_network = nn.Network(NNSIZE)
 
-        self.x = XAREA * np.random.random_sample()
-        self.y = YAREA * np.random.random_sample()
-        self.velx = 0.0
-        self.vely = 0.0
+    self.x = XAREA * np.random.random_sample()
+    self.y = YAREA * np.random.random_sample()
+    self.velx = 0.0
+    self.vely = 0.0
 
-    def generate_new_vel(self):
-        return (np.random.random_sample() - 0.5) 
+    self.time_to_goal = None
 
-    def move(self, frame):
-        print(self.x)
-        def distance(x1, y1, x2, y2):
-            return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+  def rand_position(self):
+    self.x = XAREA * np.random.random_sample()
+    self.y = YAREA * np.random.random_sample()
 
-        def inside(x1, y1):
-            if distance(x1, y1, XGOAL, YGOAL) <= RADIUSGOAL:
-                return True
-            else:
-                return False
+  def generate_new_vel(self):
+    return (np.random.random_sample() - 0.5)
 
+  def evaluate(self):
+    return self.inside()
 
-        # if dot is inside the circle it tries to maximize the distances to
-        # other dots inside circle
-        if not inside(self.x, self.y):
+  def mutate(self):
+    self.neural_network.mutate()
 
-            n_val, z = self.neural_network.feedforward(np.reshape( [self.x,self.y], (2, 1)))
+  def distance(self, x1, y1, x2, y2):
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
-            velx, vely = n_val[-1]
+  def inside(self):
+    if self.distance(self.x, self.y, XGOAL, YGOAL) <= RADIUSGOAL:
+      return True
+    else:
+      return False
 
-            self.velx = velx[0]*2-1
-            self.vely = vely[0]*2-1
+  def move(self, frame):
+    # if dot is inside the circle it stops moving
+    if not self.inside():
+      n_val, z = self.neural_network.feedforward(
+        np.reshape([self.x, self.y], (2, 1)))
 
-            self.x = self.x + self.velx
-            self.y = self.y + self.vely
+      velx, vely = n_val[-1]
 
-            # Border patrol
-            if self.x >= XAREA:
-                self.x = XAREA
-                self.velx = -1 * self.velx
-            if self.x <= 0:
-                self.x = 0.0
-                self.velx = -1 * self.velx
-            if self.y >= YAREA:
-                self.y = YAREA
-                self.vely = -1 * self.vely
-            if self.y <= 0:
-                self.y = 0.0
-                self.vely = -1 * self.vely
+      self.velx = velx[0]*2-1
+      self.vely = vely[0]*2-1
 
+      self.x = self.x + self.velx
+      self.y = self.y + self.vely
 
+      # Border patrol
+      if self.x >= XAREA:
+        self.x = XAREA
+        self.velx = -1 * self.velx
+      if self.x <= 0:
+        self.x = 0.0
+        self.velx = -1 * self.velx
+      if self.y >= YAREA:
+        self.y = YAREA
+        self.vely = -1 * self.vely
+      if self.y <= 0:
+        self.y = 0.0
+        self.vely = -1 * self.vely
+    else:
+      # print(self.time_to_goal)
+      if self.time_to_goal == None:
+        self.time_to_goal = frame
 
 
 # First set up the figure, the axis, and the plot element we want to animate
@@ -91,45 +106,62 @@ class dots(object):
     self.dots = [dot() for i in range(N)]
 
   def new(self):
+    successful_dots = []
+    for d in self.dots:
+      if d.evaluate():
+        successful_dots.append(d)
+    successful_dots.sort(key=lambda d: d.time_to_goal)
+
     self.dots = [dot() for i in range(N)]
+
+    for i in range(len(successful_dots)):
+      successful_dots[i].rand_position()
+
+      successful_dots[i].mutate()
+
+      self.dots[i] = successful_dots[i]
+
+      if i > N/NEWRANDOMRATE:
+        break
+
 
 # Initializing dots
 # dots = [dot() for i in range(N)]
 dots = dots()
 
 d, = ax.plot([dot.x for dot in dots.dots],
-            [dot.y for dot in dots.dots], 'ro')
+             [dot.y for dot in dots.dots], 'ro')
 
 
 def restart():
-    dots.new()
+  dots.new()
 
 
 # animation function.  This is called sequentially
 def animate(frame):
-  
+
   # if (frame == FRAMES):
   #   plt.close(fig)
-    # sys.exit(0)
-  print(dots.dots[0])
+  # sys.exit(0)
   for dot in dots.dots:
     dot.move(frame)
     d.set_data([dot.x for dot in dots.dots],
-              [dot.y for dot in dots.dots])
+                [dot.y for dot in dots.dots])
   return d,
 
+
 def infinite_sequence():
-    num = 0
-    while True:
+  num = 0
+  while True:
 
-        if (num == FRAMES):
-            # plt.close(fig)
-            num = 0
-            restart()
+    if (num == FRAMES):
+      # plt.close(fig)
+      num = 0
+      restart()
 
-        print(num)
-        yield num
-        num += 1
+    # print(num)
+    yield num
+    num += 1
 
 
 # call the animator.  blit=True means only re-draw the parts that have changed.
--- neural_network.py
+++ neural_network.py
@@ -1,96 +1,132 @@
 import random
 import numpy as np
 import time
+import warnings
+
+#suppress warnings
+warnings.filterwarnings('ignore')
 
 def sigmoid(z):
-    # print(z)
-    # z.astype(float)
-    # for x, i in range(len(z)):
-    #     z[i].astype(float)
-
-    return 1.0/(1.0+np.exp(-z))
+  return 1.0/(1.0+np.exp(-z))
 
 def sigmoid_prime(z):
-    return sigmoid(z)*(1-sigmoid(z))
+  return sigmoid(z)*(1-sigmoid(z))
 
 def max_index(array):
-    max_i = 0
-    i = 1
-    while (i < len(array)):
-        if (array[i] > array[max_i]):
-            max_i = i
-        i += 1
-    return max_i
+  max_i = 0
+  i = 1
+  while (i < len(array)):
+    if (array[i] > array[max_i]):
+      max_i = i
+    i += 1
+  return max_i
 
 def mean(array):
-    total = 0
-    for num in array:
-        total += num
-    mean = total / len(array)
-    return mean
+  total = 0
+  for num in array:
+    total += num
+  mean = total / len(array)
+  return mean
 
 class Network:
 
-    def __init__(self, sizes, weights=None, biases=None):
-        self.layers = len(sizes)
-        self.sizes = sizes
-        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])] if weights is None else weights 
-        self.biases = [np.random.randn(y, 1) for y in sizes[1:]] if biases is None else biases
-        self.learn_rate = 0.05
-        self.gamma = 0.99
+  def __init__(self, sizes, weights=None, biases=None):
+    self.layers = len(sizes)
+    self.sizes = sizes
+    self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])] if weights is None else weights 
+    self.biases = [np.random.randn(y, 1) for y in sizes[1:]] if biases is None else biases
+    self.learn_rate = 0.05
+    self.gamma = 0.99
+    self.mutation_rate = 0.001
+    self.bias_change_rate = 0.5
+    self.weight_change_rate = 0.5
+      
+  def feedforward(self, a):
+    values = [a]
+    all_z = [[None]]
+    for w, b in zip(self.weights, self.biases):
+      z = np.dot(w, a) + b
+      all_z.append(z)
+      a = sigmoid(z)
+      values.append(a)
+    return values, all_z
 
-        # print(self.weights)
-        
-        
-    def feedforward(self, a):
-        values = [a]
-        all_z = [[None]]
-        for w, b in zip(self.weights, self.biases):
-            z = np.dot(w, a) + b
-            all_z.append(z)
-            a = sigmoid(z)
-            values.append(a)
-        return values, all_z
+  def backprop(self, all_n_values, z_values, all_w, actions, desiered_val):
 
-    def backprop(self, all_n_values, z_values, all_w, actions, desiered_val):
+    change_w = [np.zeros(w.shape) for w in self.weights]
+    change_b = [np.zeros(b.shape) for b in self.biases]
+    steps_from_start = 0
 
-        change_w = [np.zeros(w.shape) for w in self.weights]
-        change_b = [np.zeros(b.shape) for b in self.biases]
-        steps_from_start = 0
+    #   network
+    for val, z_val, w, a in zip(all_n_values, z_values, all_w, actions):
+      l = 1
+      change_v = [np.zeros(v.shape) for v in all_n_values[0]]
+      # print(a)
+      if desiered_val == 1:
+        change_v[-1][a][0] = 1
+      else:
+        for index in range(len(change_v[-1])):
+          change_v[-1][index][0] = 1
+        change_v[-1][a][0] = 0
 
-        #   network
-        for val, z_val, w, a in zip(all_n_values, z_values, all_w, actions):
-            l = 1
-            change_v = [np.zeros(v.shape) for v in all_n_values[0]]
-            # print(a)
-            if desiered_val == 1:
-                change_v[-1][a][0] = 1
-            else:
-                for index in range(len(change_v[-1])):
-                    change_v[-1][index][0] = 1
-                change_v[-1][a][0] = 0
+      #   layer
+      while l < self.layers:
 
-            #   layer
-            while l < self.layers:
+        #   neuron
+        for i in range(len(val[-l])):
+          #   weights connected to specific neuron
+          #   j is a specific weight and also the neuron it is connected to in the previous layer
+          change_b[-l][i] -= (sigmoid_prime(z_val[-l][i]) * 2 * (val[-l][i] - change_v[-l][i][0])) * (self.gamma**steps_from_start) * self.learn_rate
+          for j in range(len(w[-l][i])):
+            change_w[-l][i][j] -= (val[(-l-1)][j] * sigmoid_prime(z_val[-l][i]) * 2 * (val[-l][i] - change_v[-l][i][0])) * (self.gamma**steps_from_start) * self.learn_rate
+            change_v[-l-1][j] -= (w[-l][i][j] * sigmoid_prime(z_val[-l][i]) * 2 * (val[-l][i] - change_v[-l][i][0])) * (self.gamma**steps_from_start) * self.learn_rate
 
-                #   neuron
-                for i in range(len(val[-l])):
-                    #   weights connected to specific neuron
-                    #   j is a specific weight and also the neuron it is connected to in the previous layer
-                    change_b[-l][i] -= (sigmoid_prime(z_val[-l][i]) * 2 * (val[-l][i] - change_v[-l][i][0])) * (self.gamma**steps_from_start) * self.learn_rate
-                    for j in range(len(w[-l][i])):
-                        change_w[-l][i][j] -= (val[(-l-1)][j] * sigmoid_prime(z_val[-l][i]) * 2 * (val[-l][i] - change_v[-l][i][0])) * (self.gamma**steps_from_start) * self.learn_rate
-                        change_v[-l-1][j] -= (w[-l][i][j] * sigmoid_prime(z_val[-l][i]) * 2 * (val[-l][i] - change_v[-l][i][0])) * (self.gamma**steps_from_start) * self.learn_rate
+        l += 1
+      steps_from_start += 1
+    #   increase/decrease each weight by calculated value
+    for layer in range(len(self.weights)):
+      for neuron in range(len(self.weights[layer])):
+        self.biases[layer][neuron] += change_b[layer][neuron]
+        for weight in range(len(self.weights[layer][neuron])):
+          self.weights[layer][neuron][weight] += change_w[layer][neuron][weight]
 
-                l += 1
-            steps_from_start += 1
-        #   increase/decrease each weight by calculated value
-        for layer in range(len(self.weights)):
-            for neuron in range(len(self.weights[layer])):
-                self.biases[layer][neuron] += change_b[layer][neuron]
-                for weight in range(len(self.weights[layer][neuron])):
-                    self.weights[layer][neuron][weight] += change_w[layer][neuron][weight]
-                    
+  def mutate(self):
+
+    for neuron_layer_i, x in enumerate(self.biases):
+      for bias_i, y in enumerate(x):
+        self.biases[neuron_layer_i][bias_i] = y + (np.random.random_sample() - 0.5) * self.bias_change_rate
+
+    for neuron_layer_i, x in enumerate(self.weights):
+      for neuron_i, y in enumerate(x):
+        for weight_i, z in enumerate(y):
+          self.weights[neuron_layer_i][neuron_i][weight_i] = z + (np.random.random_sample() - 0.5) * self.weight_change_rate
+
+
+
+
+
+    #     new_biases.each_with_index do |x, index|
+    #     x.each_with_index do |y, i|
+    #         y.each_with_index do |z, j|
+    #             unless index == 0
+    #                 new_biases[index][i][j] += (rand * (2) - 1) * change_rate
+    #             end
+    #         end 
+    #     end
+    # end
+    
+    # new_weights.each_with_index do |x, index|
+    #     x.each_with_index do |y, i|
+    #         y.each_with_index do |z, j|
+    #             z.each_with_index do |t, k|
+    #                 unless index == 0
+    #                     new_weights[index][i][j][k] += (rand * (2) - 1) * change_rate
+    #                 end
+    #             end
+    #         end 
+    #     end
+    # end               
+
 
 
 
